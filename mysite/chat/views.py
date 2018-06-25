12f106df64dac2ef46269ad9358df50af8b962d5
soham <som.insomniacs@gmail.com>
2018-06-25 17:53:03 +0530

    font change

--- mysite/chat/views.py
+++ mysite/chat/views.py
@@ -126,7 +126,7 @@ class VoiceRecordTemplate(TemplateView):
         _record.save()
 
         if request.GET.get('event', None) == 'Dial' or request.GET.get('event', None) == 'Hangup':
-            return render(request, 'chat/record.html', context={'file_name': random_with_N_digits(7)})
+            return render(request, 'chat/hangup.html', context={'file_name': random_with_N_digits(7)})
 
         from channels.layers import get_channel_layer
         channel_layer = get_channel_layer()
